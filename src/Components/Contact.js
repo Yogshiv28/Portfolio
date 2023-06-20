6f64ef7ca64d174791181fe83d12bf040f0c50e5
Yogesh <yogehshivdeys@gmail.com>
2023-06-20 19:47:56 +0530

    fifth commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -24,19 +24,7 @@ const Contact = () => {
 
       <div className="container contact__container">
         <div className="contact__options">
-          {/* <article className="contact__option">
-            <MdOutlineEmail className='contact__option-icon'/>
-            <h4>Email</h4>
-            <h5>dummyegator@gmail.com</h5>
-            <a href="mailto:dummyegator@gmail.com" target="_blank">Send a message</a>
-          </article> */}
-         
-          {/* <article className="contact__option">
-            <BsWhatsapp className='contact__option-icon'/>
-            <h4>WhatsApp</h4>
-            <h5>+123456789</h5>
-            <a href="https://api.whatsapp.com/send?phone=+1234567890" target="_blank">Send a message</a>
-          </article> */}
+          
         </div>
         {/* END OF CONTACT OPTIONS */}
         <form ref={form} onSubmit={sendEmail}>
